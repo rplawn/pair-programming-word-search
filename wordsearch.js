2db602cd3be6a2985d43deb10cdeb17444d86172
roop <rplawn@icloud.com>
2022-04-23 14:25:56 -0700

    implemented reverse word option

--- wordsearch.js
+++ wordsearch.js
@@ -1,26 +1,29 @@
 const wordSearch = (letters, word) => {
   let result = false;
+
+  // reverse the word!
+  let reverseWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverseWord += word[i];
+  }
+
   if (!word || !letters) {
     return result;
   }
   const horizontalJoin = letters.map(ls => ls.join(''));
   for (let l of horizontalJoin) {
-    if (l.includes(word)) {
+    if (l.includes(word) || l.includes(reverseWord)) {
       result = true;
       return result;
-    } else {
-      result = false;
     }
-  } 
+  }
 
   let checkVertical = letters[0].map((col, c) => letters.map((row, r) => letters[r][c]));
 
   const verticalJoin = checkVertical.map(ex => ex.join(''));
   for (let e of verticalJoin) {
-    if (e.includes(word)) {
+    if (e.includes(word) || e.includes(reverseWord)) {
       return true;
-    } else {
-      result = false;
     }
   }
     
