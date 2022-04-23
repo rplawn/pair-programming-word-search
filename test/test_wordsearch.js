756b42d4c72727b2db11cdce05e908e92d2d90a5
roop <rplawn@icloud.com>
2022-04-23 14:04:29 -0700

    functions to find words horizontally and vertically

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -7,10 +7,10 @@ describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'F', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'A', 'Q', 'U', 'A', 'L'],
       ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['W', 'H', 'C', 'K', 'Y', 'E', 'R', 'L'],
       ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,38 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  let result = false;
+  if (!word || !letters) {
+    return result;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) {
+      result = true;
+      return result;
+    } else {
+      result = false;
     }
-}
+  } 
 
-module.exports = wordSearch
+  let checkVertical = letters[0].map((col, c) => letters.map((row, r) => letters[r][c]));
+
+  const verticalJoin = checkVertical.map(ex => ex.join(''));
+  for (let e of verticalJoin) {
+    if (e.includes(word)) {
+      return true;
+    } else {
+      result = false;
+    }
+  }
+    
+  return result;
+};
+
+
+module.exports = wordSearch;
+
+
+
+
+
+
+  
